04671af1fcf27557b309201b25a1d6711bad8576
akashk12334 <akashashni930@gmail.com>
2025-12-19 14:01:03 +0530

    initial work

---
+++ OEPS/OEPS/src/main/java/com/pro/OEPS/OepsApplication.java
@@ -0,0 +1,13 @@
+package com.pro.OEPS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OepsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OepsApplication.class, args);
+	}
+
+}
