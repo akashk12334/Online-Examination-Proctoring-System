5846cef929c61b078870cb51ac1cf0155ed88f12
akashk12334 <akashashni930@gmail.com>
2025-12-19 11:49:44 +0530

    Initial Spring Boot project

---
+++ OEPS/OEPS/src/test/java/com/pro/OEPS/OepsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pro.OEPS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OepsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
